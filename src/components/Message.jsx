9435b66831d87554b0055da8bb27175479840eea
treasure odetokun <odetokuntreasure6@gmail.com>
2024-12-30 02:55:08 +0100

    removed the other

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -5,13 +5,13 @@ function Message() {
         Hi baby, this is to thank you for being so amazing throughout the
         year❤️. The crazy thing is there are so many things to thank you for so
         I don’t even know where to start from but I guess I’ll start from how I
-        was so serious with my plan of not entering any other relationship this
-        year till 2025 not until I met you, like I usually say that you used
-        jazz for me but for real you won me over with your gentle love. I love
-        how you’re so gentle with me, so kind and so caring about anything that
-        concerns me. You’re literally the biggest highlight of my 2024 which
-        makes me so thankful I went out that night we met cos I don’t even know
-        how my year would have been without you in it.
+        was so serious with my plan of not entering any relationship this year
+        till 2025 not until I met you, like I usually say that you used jazz for
+        me but for real you won me over with your gentle love. I love how you’re
+        so gentle with me, so kind and so caring about anything that concerns
+        me. You’re literally the biggest highlight of my 2024 which makes me so
+        thankful I went out that night we met cos I don’t even know how my year
+        would have been without you in it.
       </p>
     </div>
   );
